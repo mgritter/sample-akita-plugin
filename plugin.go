d96309eab057616d2867695334b7395402138ffb
Mark Gritter <mgritter@akitasoftware.com>
2021-08-20 16:32:45 -0500

    An example Akita CLI plugin.

---
+++ plugin.go
@@ -0,0 +1,31 @@
+package sample_akita_plugin
+
+import (
+	"github.com/akitasoftware/akita-cli/plugin"
+	"github.com/akitasoftware/akita-libs/spec_util"
+	pb "github.com/akitasoftware/akita-ir/go/api_spec"
+	"strings"
+)
+
+type SillyAkitaPlugin struct{}
+
+func (s SillyAkitaPlugin) Name() string {
+	return "SillyAkitaPlugin"
+}
+
+func (s SillyAkitaPlugin) Transform(method *pb.Method) error {
+	meta := spec_util.HTTPMetaFromMethod(method)
+	if meta == nil {
+		return nil
+	}
+	path := strings.Split( meta.PathTemplate, "/" )
+	for i := 1; i < len(path); i++ {
+		path[i] = "bork"
+	}
+	meta.PathTemplate = strings.Join( path, "/" )
+	return nil
+}
+
+func LoadAkitaPlugin() (plugin.AkitaPlugin, error) {
+	return SillyAkitaPlugin{}, nil
+}
